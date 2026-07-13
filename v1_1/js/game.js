75c84c24bd2b9870237097633a9ad1fe25e9e54e
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-13 17:40:18 +0900

    Update game.js

--- v1_1/js/game.js
+++ v1_1/js/game.js
@@ -1,15 +1,59 @@
+import { Maze } from "./maze.js";
+import { Eel } from "./eel.js";
+import { Input } from "./input.js";
+import { Renderer } from "./renderer.js";
+
 export class Game {
 
-    constructor(){
+    constructor() {
 
         this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext("2d");
 
+        // 各クラス生成
+        this.maze = new Maze(this);
+        this.eel = new Eel(this);
+        this.input = new Input(this);
+        this.renderer = new Renderer(this);
+
+        // リサイズイベント
+        window.addEventListener("resize", () => this.resize());
+
     }
 
-    start(){
+    start() {
 
-        console.log("Game Start");
+        this.resize();
+        this.loop();
+
+    }
+
+    resize() {
+
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+    }
+
+    update() {
+
+        // 次工程で実装
+
+    }
+
+    draw() {
+
+        this.renderer.draw();
+
+    }
+
+    loop() {
+
+        this.update();
+
+        this.draw();
+
+        requestAnimationFrame(() => this.loop());
 
     }
 
