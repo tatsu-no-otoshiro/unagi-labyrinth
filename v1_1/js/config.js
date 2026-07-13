3c368a6a907f67c0eb6e4464637f16744c01a542
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-13 19:03:27 +0900

    Create config.js

---
+++ v1_1/js/config.js
@@ -0,0 +1,25 @@
+export const CONFIG = {
+
+    // 迷路
+    MAP_SIZE: 15,
+    MAP_MARGIN: 40,
+
+    // タイル
+    TILE_SIZE_MIN: 32,
+
+    // ウナギ
+    EEL_SPEED: 2.5,
+    EEL_RADIUS: 8,
+
+    // ゴール
+    GOAL_RADIUS: 10,
+
+    // 色
+    COLORS: {
+        BACKGROUND: "#9ed8ff",
+        WALL: "#355",
+        GOAL: "gold",
+        EEL: "#643"
+    }
+
+};
