9bd1fb90a7bedc511fb1eee8d9c5cb817f8d88c5
タツノオトシロウ <yocchi.biz@gmail.com>
2026-07-29 18:01:04 +0900

    Update renderer.js

--- v1_7/js/renderer.js
+++ v1_7/js/renderer.js
@@ -10,8 +10,6 @@ export class Renderer {
 
     draw() {
 
-        const game = this.game;
-
         const ctx = this.game.ctx;
         const canvas = this.game.canvas;
 
@@ -149,11 +147,6 @@ export class Renderer {
 
         // 頭
         this.drawHead(ctx, eel);
-
-        // クリア演出
-        if (game.isCleared) {
-            this.drawClearOverlay(ctx, game);
-        }
 
     }
 
@@ -542,47 +535,6 @@ export class Renderer {
 	ctx.fill();
 
         ctx.restore();
-    }
-
-    /**
-     * クリア画面を描画
-     * @param {CanvasRenderingContext2D} ctx
-     * @param {Game} game
-     */
-    drawClearOverlay(ctx, game) {
-
-        const w = game.canvas.width;
-        const h = game.canvas.height;
-
-        // 半透明背景
-        ctx.fillStyle = "rgba(0,0,0,0.45)";
-        ctx.fillRect(0, 0, w, h);
-
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-
-        // CLEAR!
-        ctx.fillStyle = "#ffd54f";
-        ctx.font = "bold 56px sans-serif";
-        ctx.fillText("CLEAR!", w / 2, h / 2 - 60);
-
-        // タイム
-        ctx.fillStyle = "white";
-        ctx.font = "32px sans-serif";
-        ctx.fillText(
-            `${game.clearTime.toFixed(2)} sec`,
-            w / 2,
-            h / 2 + 5
-        );
-
-        // リトライ案内
-        ctx.fillStyle = "#dddddd";
-        ctx.font = "22px sans-serif";
-        ctx.fillText(
-            "Tap / Click to Retry",
-            w / 2,
-            h / 2 + 60
-        );
     }
 
 }
